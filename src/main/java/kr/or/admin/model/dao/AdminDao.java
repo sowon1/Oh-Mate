39946f19e32ff5e908577348f7cc59ea73945eaa
user1 <user1@user1-PC>
2021-11-30 15:16:17 +0900

    처리일시 : 211130 처리자 : 원보현 처리 내역 :
    1. admin 관련 세팅
    - controller, service, dao 패키지, 클래스 생성
    - servlet-context 스캔 패키지 경로 추가
    
    2. 회원관리 jsp 생성

---
+++ src/main/java/kr/or/admin/model/dao/AdminDao.java
@@ -0,0 +1,11 @@
+package kr.or.admin.model.dao;
+
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class AdminDao {
+	@Autowired
+	private SqlSessionTemplate sqlSession;
+}
