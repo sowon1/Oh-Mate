bfcad1dc43a0c42c826eabfbeccf0ff9ce01ad3a
Administrator <Administrator@192.168.200.109>
2021-12-01 17:37:14 +0900

    처리일시 : 211201 처리자 : 원보현 처리 내역 : Member.java - VO 수정

--- src/main/java/kr/or/member/vo/Member.java
+++ src/main/java/kr/or/member/vo/Member.java
@@ -4,9 +4,16 @@ import lombok.Data;
 
 @Data
 public class Member {
+	private int memberNo;
 	private String memberId;
 	private String memberPw;
 	private String memberName;
-	private String address;
+	private String phone;
+	private String email;
 	private String enrollDate;
+	private String filepath;
+	private int memberLevel;
+	private int gender;
+	private int age;
+	private int profileStatus;
 }
