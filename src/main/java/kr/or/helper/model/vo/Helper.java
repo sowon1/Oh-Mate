84b6289f653edf1990747f602d27935090688ee1
thswotlrsla <thswotlrsla@naver.com>
2021-12-01 17:21:48 +0900

    작업일자: 2021/12/01
    작업자: 윤지성
    
    작업내용
    1. VO >> helper.class 요소정리 및  getter setter 정리

--- src/main/java/kr/or/helper/model/vo/Helper.java
+++ src/main/java/kr/or/helper/model/vo/Helper.java
@@ -1,5 +1,19 @@
 package kr.or.helper.model.vo;
 
-public class Helper {
+import lombok.Data;
 
+@Data
+public class Helper {
+	private int helperNo;				//헬퍼번호
+	private int memberNo;				//헬퍼요청할때 쓰는 회원번호
+	private String helperName;			//닉네임
+	private String helperCategory;		//심부름 항목
+	private String helperStartTime;		//hh:mm 선호시작시간
+	private String helperEndTime;		//hh:mm 선호 마감시간
+	private int helperRide;				// 이동수단
+	private int helperStatus;			// 1. 대기중/ 2. 승인/ 3. 미승인
+	private String helperFilepath;		// 프로필사진(증빙용)
+	private String helperIntro;			//자기소개
+	private String helperCredit;		//헬퍼 자격증 사진
+	private String helperRefuse;		//헬퍼 미승인 사유
 }
