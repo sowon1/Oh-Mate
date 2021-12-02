e6a105e538bb82c446cd62d71ba5737853bc6fe6
user1 <user1@user1-PC>
2021-12-02 18:00:41 +0900

    처리일시 : 211202 처리자 : 정진환 처리 내역 :  noticeList.css작성  noticeList 작성

--- src/main/java/kr/or/notice/controller/NoticeController.java
+++ src/main/java/kr/or/notice/controller/NoticeController.java
@@ -1,10 +1,14 @@
 package kr.or.notice.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.notice.model.service.NoticeService;
+import kr.or.notice.model.vo.Notice;
 
 
 @Controller
@@ -12,6 +16,11 @@ public class NoticeController {
 
 	@Autowired
 	private NoticeService service;
+	
+	@RequestMapping(value = "/noticeList.do")
+	public String noticeList(int reqPage, Model model) {
+		return "notice/noticeList";
+	}
 	
 	@RequestMapping(value = "/noticeWriteFrm.do")
 	public String noticeWriteFrm() {
