2390ad29a62395d936a81e7a7df95ebcedc14ab8
thswotlrsla <thswotlrsla@naver.com>
2021-12-02 15:38:40 +0900

    작업일: 2021/12/02 작업자: 윤지성
    작업내용:
    1. helperRequestFrm.jsp datepick 적용 및 컨트롤러 적용

--- src/main/java/kr/or/helper/controller/HelperController.java
+++ src/main/java/kr/or/helper/controller/HelperController.java
@@ -2,13 +2,21 @@ package kr.or.helper.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.or.helper.model.service.HelperService;
+import kr.or.member.model.vo.Member;
 
 @Controller
 public class HelperController {
 	@Autowired
 	private HelperService service;
 	
+	//헬퍼 요청양식 이동
+	@RequestMapping(value = "/helperRequestFrm.do")
+	public String helperRequestFrm(Model model) {
+		return "helper/helperRequestFrm";
+	}
 
 }
