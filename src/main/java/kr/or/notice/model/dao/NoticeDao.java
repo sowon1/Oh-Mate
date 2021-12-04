dec95693aa7f8abb7d9f518e85625e0e902fc985
jinhwan <jinhwan@DESKTOP-4HOBROM>
2021-12-04 12:09:10 +0900

    처리일시 : 211204 처리자 : 정진환 처리 내역 :
    1. header.jsp
     - noticeList.do경로에 ?reqPage=1추가
    
    2. resources - summernote추가
    
    3.
    noticeWriteFrm.jsp 작성
    noticeWriteFrm.css 생성

--- src/main/java/kr/or/notice/model/dao/NoticeDao.java
+++ src/main/java/kr/or/notice/model/dao/NoticeDao.java
@@ -1,10 +1,13 @@
 package kr.or.notice.model.dao;
 
 import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class NoticeDao {
+	
+	@Autowired
 	private SqlSessionTemplate sqlSession;
 	
 }
