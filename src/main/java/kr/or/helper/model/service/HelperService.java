6192f1c4ae152f5865dacdfda9010782c8b102d9
thswotlrsla <thswotlrsla@naver.com>
2021-12-14 18:21:10 +0900

    작업일자 2021/12/14 작업자 윤지성 작업내용 1.헬퍼 등록 폼 수정 및 헬퍼 요청 등록기능 구현중 (15%) 2. helperSQL 추가 , typeAlias helper추가

--- src/main/java/kr/or/helper/model/service/HelperService.java
+++ src/main/java/kr/or/helper/model/service/HelperService.java
@@ -1,12 +1,24 @@
 package kr.or.helper.model.service;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.or.helper.model.dao.HelperDao;
+import kr.or.helper.model.vo.Helper;
 
 @Service
 public class HelperService {
 	@Autowired
 	private HelperDao dao;
-}
+
+	public int helperRequestNoImg(Helper h, String[] addressCode, String[] addressName, String[] addressRoad,
+			String addressLegal) {
+			
+			
+		return 0;
+	}
+	
+	}
+
