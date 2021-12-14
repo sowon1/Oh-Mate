cba3d09a44582e517a9dc89101c19013876c6ef4
k <k@LAPTOP-2IRC8ID6>
2021-12-14 17:22:39 +0900

    처리자 : 박소원 처리일시 : 21-12-14 처리 내역 :
    1. House, Room
    ㄴ 입주 신청 기능 구현 완료, 입주 신청시 입주테이블 등록,
    방 테이블에서 방 상태 입주불가로 업데이트, 결제 테이블에 정보 등록

--- src/main/java/kr/or/chatmsg/model/vo/ChatMsg.java
+++ src/main/java/kr/or/chatmsg/model/vo/ChatMsg.java
@@ -4,7 +4,8 @@ import lombok.Data;
 
 @Data
 public class ChatMsg {
-	private int messageNo;			//채팅방번호 (채팅리스트번호 참조 / 시퀀스 절대아님)
+	private int messageNo;			//채팅방 번호 시퀀스
+	private int chatNo;			//채팅 번호 참조하는거 (채팅리스트번호 참조 / 시퀀스 절대아님)
 	private int sender;				//보낸사람
 	private String messageContent;	//메세지
 	private String messageDate;		//보낸시간 YYYY-MM-DD- HH:MM
